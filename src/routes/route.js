154dff60054f38f752e1c11d0bb1a0ec75185369
Anand Sharma <sharmaanand570@gmail.com>
2022-06-02 16:11:57 +0530

    assignment/nodejs-module with problem-4

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const externalModule = require('../logger/logger')
 const helperModule = require("../util/helper")
 const validatorModule = require("../validator/formatter")
+const lodash = require("lodash")
 
 const router = express.Router();
 
@@ -14,6 +15,20 @@ router.get('/test-me',function(req, res){
     validatorModule.trim()
     validatorModule.changetoLowerCase()
     validatorModule.changetoUpperCase()
+})
+
+router.get('/hello',function (req, res){
+    const allMonths = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
+    console.log(lodash.chunk(allMonths,4))
+
+    const oddNumbers = [1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(oddNumbers))
+
+    const numbers = [1,2,5,5,2]
+    console.log(lodash.union(numbers))
+
+    const movies = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    console.log(lodash.fromPairs(movies))
 })
 
 module.exports = router;
--- src/util/helper.js
+++ src/util/helper.js
@@ -3,15 +3,12 @@ const printDate = function(){
     let day = date.getDate()
     console.log(day)
 }
-printDate()
 const printMonth = function(){
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
     const date = new Date()
     let currentMonth = month[date.getMonth()]
     console.log(currentMonth)
 }
-
-printMonth()
 const getBatchinfo = function(){
     console.log("Radon, W3D3, the topic for today is Nodejs module system")
 }
