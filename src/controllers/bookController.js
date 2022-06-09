bdc3106a37b70d7ee0c8f92b01826e871658bcaa
Anand Sharma <sharmaanand570@gmail.com>
2022-06-10 00:22:03 +0530

    assignment mongoose populate and reference

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,10 +1,35 @@
-const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const authorModel= require("../models/authorModel")
+const publisherModel = require("../models/publisherModel")
 
 const createBook= async function (req, res) {
     let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+    let authorId = book.author
+    if(authorId && authorId.length==24){
+        let authorIdData = await authorModel.find({_id:authorId})
+        if(authorIdData.length==0){
+            res.send({msg:"the author is not present"})
+        }
+        else{
+            let publisherId = book.publisher
+            if(publisherId && publisherId.length==24){
+                let publisherIdData = await publisherModel.find({_id:publisherId})
+                if(publisherIdData.length==0){
+                    res.send({msg: "the publisher is not present"})
+                }
+                else{
+                    let bookCreated = await bookModel.create(book)
+                    res.send({data: bookCreated})
+                }
+            }
+            else{
+                res.send({msg: "publisher Id is not present"})
+            }
+        }
+    }
+    else{
+       res.send({msg:"author Id is not present."})
+    }
 }
 
 const getBooksData= async function (req, res) {
@@ -12,12 +37,22 @@ const getBooksData= async function (req, res) {
     res.send({data: books})
 }
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+const getBooksWithAuthorAndPublisherDetails = async function (req, res) {
+    let specificBook = await bookModel.find().populate('author').populate('publisher')
     res.send({data: specificBook})
+}
 
+const updateBookPrice = async function (req, res){
+    let findAuthor = await authorModel.find({rating: {$gt:3.5}}).select("_id")
+    let BookPriceUpdate = []
+    for(let i=0;i<findAuthor.length;i++){
+        BookPriceUpdate.push(await bookModel
+            .updateMany({author:findAuthor[i]},{$inc:{price:10}},{new:true}))
+    }
+    res.send({msg: BookPriceUpdate})
 }
 
 module.exports.createBook= createBook
 module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.getBooksWithAuthorAndPublisherDetails = getBooksWithAuthorAndPublisherDetails
+module.exports.updateBookPrice = updateBookPrice
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://radon-AnandSharma:Anand570@cluster0.riu1k.mongodb.net/Anand570-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
