71465a75a0edb5463c1226b148941494dea3238e
Anand Sharma <sharmaanand570@gmail.com>
2022-06-10 20:55:57 +0530

    assignment middleware - 1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-var bodyParser = require('body-parser');
+const moment = require('moment')
 
+var bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 
 const app = express();
@@ -9,6 +10,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
+
+app.use(function (req, res){
+    currentDateAndTime = moment().format("YYYY-MM-DD HH:MM:SS")
+    currentIP = req.ip
+    cuurentRequestPath = req.path
+    console.log(currentDateAndTime,",", currentIP,",",cuurentRequestPath)
+})
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/test-me', function (req, res, next) {
     res.send('My first ever api!')
+    next()
 });
 
+router.get('/middleware', function (req, res, next){
+    res.send("this is middleware")
+    next()
+})
+
+
 module.exports = router;
+
 // adding this comment for no reason
